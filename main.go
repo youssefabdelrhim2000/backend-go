249e1dbe50a3d01774090e2d6d768edf42f9cd04
youssefabdelrhim2000 <144361815+youssefabdelrhim2000@users.noreply.github.com>
2026-05-02 08:13:19 -0700

    Update main.go

--- main.go
+++ main.go
@@ -103,7 +103,7 @@ func main() {
     
     // CORS أولاً
     app.Use(cors.New(cors.Config{
-        AllowOrigins:  []string{"http://localhost:5173", "http://localhost:3000"},
+        AllowOrigins:  []string{"https://porjets-2iaj.vercel.app"},
         AllowMethods:  []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
         AllowHeaders:  []string{"Origin", "Content-Type", "Accept", "Authorization"},
         ExposeHeaders: []string{"Content-Disposition"},
